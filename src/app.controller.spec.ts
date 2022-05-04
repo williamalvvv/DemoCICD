202992250f9be233cf8fde3510c7f62cacf76c8e
William Alvarado <williamalvvv@Williams-MacBook-Pro.local>
2022-05-03 21:00:10 -0600

    Unit test corrected

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getSalute()).toBe('Hello Worlddd!');
+      expect(appController.getSalute()).toBe('Hello World!');
     });
   });
 });
